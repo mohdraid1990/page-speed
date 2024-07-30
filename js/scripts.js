58d91263cbe3afc882aad0f3fffe846473c379c6
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2024-07-30 22:30:26 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,75 @@
-$(document).ready(function() {
-    
-    $('.feedback').click(function() {
-        $('.popup-window').show();
+$(document).ready(function () {
+  function a(e) {
+    return (
+      "<span>" +
+      (e = ("00" + e).substr(-2))[0] +
+      "</span><span>" +
+      e[1] +
+      "</span>"
+    );
+  }
+  $(".feedback").click(function () {
+    $(".popup-window").show();
+  }),
+    $(".close-popup").click(function () {
+      $(".popup-window").hide();
+    }),
+    $("a[href^='#']").click(function () {
+      var e = $(this).attr("href");
+      return (
+        $("html, body").animate({ scrollTop: $(e).offset().top + "px" }), !1
+      );
+    }),
+    (function e() {
+      var t = new Date(),
+        o = new Date();
+      o.setHours(23),
+        o.setMinutes(59),
+        o.setSeconds(59),
+        23 === t.getHours() &&
+          59 === t.getMinutes() &&
+          59 === t.getSeconds &&
+          o.setDate(o.getDate() + 1);
+      var n = Math.floor((o.getTime() - t.getTime()) / 1e3),
+        o = Math.floor(n / 3600);
+      n -= 3600 * o;
+      t = Math.floor(n / 60);
+      (n -= 60 * t),
+        $(".timer .hours").html(a(o)),
+        $(".timer .minutes").html(a(t)),
+        $(".timer .seconds").html(a(n)),
+        setTimeout(e, 200);
+    })();
+}),
+  $(window).on("load", function () {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: !0,
+      autoHeight: !0,
+      smartSpeed: 300,
+      mouseDrag: !1,
+      pullDrag: !1,
+      dots: !1,
+      nav: !0,
+      navText: "",
     });
-    $('.close-popup').click(function() {
-        $('.popup-window').hide();
-    });
-	
-	/* scroll */
-	
-	$("a[href^='#']").click(function(){
-		var _href = $(this).attr("href");
-		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-		return false;
-	});
-
-	/* timer */
-
-	function update() {
-		var Now = new Date(), Finish = new Date();
-		Finish.setHours( 23);
-		Finish.setMinutes( 59);
-		Finish.setSeconds( 59);
-		if( Now.getHours() === 23  &&  Now.getMinutes() === 59  &&  Now.getSeconds === 59) {
-			Finish.setDate( Finish.getDate() + 1);
-		}
-		var sec = Math.floor( ( Finish.getTime() - Now.getTime()) / 1000);
-		var hrs = Math.floor( sec / 3600);
-		sec -= hrs * 3600;
-		var min = Math.floor( sec / 60);
-		sec -= min * 60;
-		$(".timer .hours").html( pad(hrs));
-		$(".timer .minutes").html( pad(min));
-		$(".timer .seconds").html( pad(sec));
-		setTimeout( update, 200);
-	}
-	function pad(s) {
-		s = ("00"+s).substr(-2);
-		return "<span>" + s[0] + "</span><span>" + s[1] + "</span>";
-	}
-	update();
-
-});
-
-$(window).on("load", function(){
-	
-	/* sliders */
-
-	$(".owl-carousel").owlCarousel({
-		items: 1,
-		loop: true,
-		autoHeight: true,
-		smartSpeed: 300,
-		mouseDrag: false,
-		pullDrag: false,
-		dots: false,
-		nav: true,
-		navText: ""
-	});
-	
-});
-
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-ym(71396785, "init", {
-    clickmap:true,
-    trackLinks:true,
-    accurateTrackBounce:true,
-    webvisor:true
-});
+  }),
+  (function (e, t, o, n, a) {
+    (e.ym =
+      e.ym ||
+      function () {
+        (e.ym.a = e.ym.a || []).push(arguments);
+      }),
+      (e.ym.l = +new Date()),
+      (n = t.createElement(o)),
+      (a = t.getElementsByTagName(o)[0]),
+      (n.async = 1),
+      (n.src = "https://mc.yandex.ru/metrika/tag.js"),
+      a.parentNode.insertBefore(n, a);
+  })(window, document, "script"),
+  ym(71396785, "init", {
+    clickmap: !0,
+    trackLinks: !0,
+    accurateTrackBounce: !0,
+    webvisor: !0,
+  });
